426ab069e8e2c8f32a34decfda12e7b9aab460f7
Richard Neher <richard.neher@unibas.ch>
2018-05-02 01:25:57 +0200

    use s instead of log NS

--- src/run_jobs_scaled.py
+++ src/run_jobs_scaled.py
@@ -5,7 +5,7 @@ for logsou in [1,2,3,4,5]:
 	for s in [0.005, 0.01, 0.02, 0.03, 0.05]:
 		for N0 in [1e9]:
 			for tc in [20, 50, 100, 200, 500, 1000, 2000]:
-				cmd = ["src/pop2pop_cluster.py", "--logsou", logsou, "--logNs", logNs, "--cross", tc,
+				cmd = ["src/pop2pop_cluster.py", "--logsou", logsou, "-s", s, "--cross", tc,
 					   "--N0", N0, "--niter", niter, "--nrep", nrep]
 				cmd_str = "sbatch submit_script.sh " + " ".join(map(str, cmd))
 				print(cmd_str)
