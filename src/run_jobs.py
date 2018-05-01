42490b450a0ba5583aeaa18d9de3b250bb843efb
Richard Neher <richard.neher@unibas.ch>
2018-05-01 18:52:07 +0200

    run script...

--- src/run_jobs.py
+++ src/run_jobs.py
@@ -7,6 +7,6 @@ for s in [0.005, 0.01, 0.02, 0.05]:
 			for tc in [20, 50, 100, 200, 500]:
 				cmd = ["src/pop2pop_cluster.py", "-u", u, "-s", s, "--cross", tc,
 					   "--N0", N0, "--niter", niter, "--nrep", nrep]
-				cmd_str = " ".join(map(str, cmd))
+				cmd_str = "sbatch submit_script.sh " + " ".join(map(str, cmd))
 				print(cmd_str)
-				os.system("./submit_script.sh "+cmd_str)
+				os.system(cmd_str)
